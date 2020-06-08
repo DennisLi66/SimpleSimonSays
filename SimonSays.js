f28e46bde85e0dd701480fcd7fcd4c030e921a42
Dennis Li <li.dennis.beta@gmail.com>
2020-06-07 21:57:59 -0700

    addsb

---
+++ SimonSays.js
@@ -0,0 +1,4 @@
+function drawBasic(){
+    document.getElementById("cTop").innerHTML = "<canvas id='c1' style='border:1px solid #d3d3d3;'> Not Supported</canvas>";
+    document.getElementById('cLeft').innerHTML = "<canvas id='cL' style='border:1px solid #d3d3d3;'> Not Supported</canvas>";
+}
